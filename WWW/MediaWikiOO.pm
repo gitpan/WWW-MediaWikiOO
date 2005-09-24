6cdb17cedaaa3813cdb4b645050cc402c70d86f7
Claus Färber <CFAERBER@cpan.org>
2005-09-24 16:57:59 +0000

    Import of CFAERBER/WWW-MediaWikiOO-0.00_0001 from CPAN.
    gitpan-cpan-distribution: WWW-MediaWikiOO
    gitpan-cpan-version:      0.00_0001
    gitpan-cpan-path:         CFAERBER/WWW-MediaWikiOO-0.00_0001.tar.gz
    gitpan-cpan-author:       CFAERBER
    gitpan-cpan-maturity:     developer

---
+++ WWW/MediaWikiOO.pm
@@ -0,0 +1,185 @@
+# $Id: MediaWikiOO.pm,v 1.2 2005/09/17 22:20:35 cfaerber Exp $
+#
+# WWW::MediaWikiOO - object-orientated interface to MediaWiki sites.
+# (c) Copyright 2005 Claus Faerber <perl@faerber.muc.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of either:
+# 
+# a) the GNU General Public License as published by the Free Software
+# Foundation; either version 1, or (at your option) any later version,
+# or
+# b) the "Artistic License" which comes with this Kit.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either the
+# GNU General Public License or the Artistic License for more details.
+# 
+# You should have received a copy of the Artistic License with this Kit,
+# in the file named "Artistic".
+# 
+# You should also have received a copy of the GNU General Public License
+# along with this program in the file named "Copying". If not, write to
+# the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA 02111-1307, USA or visit their web page on the internet at
+# http://www.gnu.org/copyleft/gpl.html.
+#
+package WWW::MediaWikiOO;
+
+our $VERSION = '0.00_0001';
+
+use utf8;
+use strict;
+use Carp;
+
+use File::Spec;
+use HTML::LinkExtor;
+use LWP::UserAgent;
+use URI;
+use URI::Heuristic;
+use URI::QueryParam;
+
+use WWW::MediaWikiOO::Article;
+
+sub new {
+  my $pkg = shift;
+  my $self = {}; bless($self, $pkg);
+
+  $self->site_uri(shift) if @_;
+  $self->logon(@_) if @_;
+
+  return $self;
+}
+
+sub _ua {
+  my ($self,$new_ua) = @_;
+  my $old_ua = $self->{'_ua'};
+
+  if($new_ua) {
+  
+    carp 'not a LWP::UserAgent' unless UNIVERSAL::isa('LWP::UserAgent',$new_ua);
+    $self->{'_ua'} = $new_ua;
+    
+  } elsif(!$old_ua) {
+  
+    $new_ua = new LWP::UserAgent(
+  	'agent'		=> 'libwww-mediawiki-perl/0.01',
+  	'cookie_jar' 	=> {}, );
+    return $self->{'_ua'} = $new_ua;
+
+  }
+
+  return $old_ua;
+}
+
+my %_api_cache;
+
+sub _api_uri {
+  my ($self,$new_api_uri) = @_;
+  my $old_api_uri = $self->{'_api_uri'};
+
+  if($new_api_uri) {
+
+    $self->{'_api_uri'} = URI->new($new_api_uri)->canonical;
+
+  } elsif(!$old_api_uri) {
+
+    return $_api_cache{$self->site_uri} if $_api_cache{$self->site_uri};
+
+    my $ok = undef;
+    my $lex = undef;
+    my $res = $self->_ua->get($self->site_uri, ':content_cb' =>
+      sub { 
+        unless($lex) {
+          my $auth =  new URI($_[1]->base)->canonical->authority;
+          $lex = HTML::LinkExtor->new(
+            sub {
+              my($tag, %attr) = @_;
+              return if $tag ne 'a' || !exists $attr{'href'};
+              my $nu = URI->new($attr{'href'})->canonical;
+   	      if($nu->query_param('title') && $nu->query_param('action') &&
+	          $nu->authority eq $auth) {
+	        $new_api_uri = $nu->as_string;
+		$new_api_uri =~ s/[\?#].*//;
+		$new_api_uri = new URI($new_api_uri)->canonical;
+	        $ok = 1;die('done');
+	      }
+            }, $_[1]->base);
+        };
+        $lex->parse($_[0])
+      });
+    carp 'cannot determine API URI - '.$res->status_line unless $ok;
+
+    $self->{'_api_uri'} = $new_api_uri;
+    $_api_cache{$self->site_uri} = $new_api_uri;
+    return $new_api_uri->clone;
+
+  }
+  return $old_api_uri->clone;
+}
+
+sub _api {
+  my($self,%param) = @_;
+  my $new = $self->_api_uri;
+  $new->query_form(%param);
+  return $new;
+}
+
+sub site_uri {
+  my ($self,$new_site) = @_;
+  my $old_site = $self->{'_site_uri'};
+
+  if($new_site)
+  {
+    my $uri = URI::Heuristic::uf_uri($new_site)->canonical;
+    carp 'unsupported uri format - ' .$uri unless $uri->scheme =~ m/^https?/i;
+    $self->{'_site_uri'} = $uri;
+    $self->{'_api_uri'} = undef;
+  }
+
+  return $old_site;
+}
+
+sub login {
+  my($self,$name,$pass) = @_;
+
+  my $res = $self->_ua->post($self->_api('title'=>'Special:Userlogin','action'=>'submit'),
+    { 'wpName' => $name, 'wpRemember' => 1, 'wpPassword' => $pass, 'wpLoginattempt' => 1, } );
+  return 1 if ($res->code >= 301 && $res->code <= 306);
+  croak $res->message unless $res->is_success;
+  croak 'login failed';
+}
+
+sub article {
+  my ($self,$title) = @_;
+  return WWW::MediaWikiOO::Article->new($self,$title);
+}
+
+sub edit_article {
+  my ($self,$title) = @_;
+  return WWW::MediaWikiOO::Article->edit_new($self,$title);
+}
+
+sub upload {
+  my ($self,$file,$message,%param) = @_;
+  $param{'filename'} ||= $file; 
+  $param{'filename'} = (File::Spec->splitpath($param{'filename'}))[2];
+
+  croak 'no message' unless $message;
+ 
+  my %data = ();
+  $data{'UploadFile'}		= (ref $file eq 'ARRAY') ? $file : [$file];
+  $data{'UploadDescription'}	= $message;
+  $data{'UploadAffirm'}		= 1;
+  $data{'Upload'}		= 1;
+  $data{'IgnoreWarning'}	= 1 if  $param{'overwrite'};
+  
+  my $res = $self->_ua->post($self->_api('title'=>'Special:Upload'),\%data );
+  
+  return 1 if ($res->code >= 301 && $res->code <= 306);
+  croak $res->message unless $res->is_success;
+  croak 'login failed';
+}
+
+1;
---
+++ WWW/MediaWikiOO/Article.pm
@@ -0,0 +1,207 @@
+# $Id: Article.pm,v 1.2 2005/09/17 22:20:35 cfaerber Exp $
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of either:
+# 
+# a) the GNU General Public License as published by the Free Software
+# Foundation; either version 1, or (at your option) any later version,
+# or
+# b) the "Artistic License" which comes with this Kit.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either the
+# GNU General Public License or the Artistic License for more details.
+# 
+# You should have received a copy of the Artistic License with this Kit,
+# in the file named "Artistic".
+# 
+# You should also have received a copy of the GNU General Public License
+# along with this program in the file named "Copying". If not, write to
+# the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA 02111-1307, USA or visit their web page on the internet at
+# http://www.gnu.org/copyleft/gpl.html.
+#
+package WWW::MediaWikiOO::Article;
+
+use strict;
+use utf8;
+
+use Carp;
+use Data::Dumper;
+use Digest::SHA1;
+use HTML::LinkExtor;
+use LWP::UserAgent;
+use URI;
+use URI::Heuristic;
+use URI::QueryParam;
+
+sub new {
+  my $pkg = shift;
+  my $self = {}; bless($self, $pkg);
+  
+  $self->site(shift);
+  $self->title(shift);
+
+  return $self;
+}
+
+sub edit_new {
+  my $self = new(@_);
+  $self->edit();
+  return $self;
+}
+
+sub _ua { 
+  return shift->site->_ua 
+}
+
+sub _api { 
+  my ($self,%param) = @_;
+  $param{'title'} = $self->title;
+  my $x = $self->site->_api(%param);
+  print STDERR "<$x>\n";
+  return $x;
+}
+
+sub title {
+  my ($self,$new_title) = @_;
+  my $old_title = $self->{'_title'};
+
+  if($new_title)
+  {
+    carp 'invalid title' if $new_title =~ m/[\[\]\+\{\}\<\>\|]/;
+    $new_title =~ s/ /_/g;
+    $self->{'_title'} = $new_title;
+  }
+  return $old_title;
+}
+
+sub site {
+  my ($self,$new_site) = @_;
+  my $old_site = $self->{'_site'};
+
+  if($new_site)
+  {
+    carp 'not a WWW::Mediawiki' unless eval{$new_site->isa('WWW::MediaWikiOO')};
+    $self->{'_site'} = $new_site;
+  }
+  return $old_site;
+}
+
+sub content_ref {
+  my($self,$new_content_ref) = @_;
+
+  if($self->editable()) {
+  
+    $self->_retrieve_content unless ($self->{'_content_sha1'});
+
+    my $old_content_ref = $self->{'_content_ref'};
+    $self->{'_content_ref'} = ref $new_content_ref ? $new_content_ref : \$new_content_ref if(defined $new_content_ref);
+    return $old_content_ref;
+    
+  } else {
+
+    carp 'read only - not editable' if $new_content_ref;
+  
+    # just get the raw data directly (not via edit form)
+    unless ($self->{'_content_ref'}) {
+      my $res = $self->_ua->get($self->_api('action'=>'raw'));
+      $self->{'_content_ref'} = $res->content_ref;
+      $self->{'_content_sha1'} = undef;
+    }
+    return $self->{'_content_ref'};
+  }
+}
+
+sub content : lvalue {
+  my($self,$new_content) = (@_);
+  ${$self->content_ref($new_content)};
+};
+
+sub content_html {
+  my($self) = @_;
+  unless($self->{'_content_html'}) {
+    my $res = $self->_ua->get($self->_api('action'=>'raw'));
+    $self->{'_content_html'} = $res->content_ref;
+  }
+  return ${$self->{'_content_html'}};
+}
+
+sub edit {
+  my($self) = @_;
+  $self->{'_content_sha1'} = undef;			# trigger reload
+  $self->editable(1);					# set editable
+}
+
+sub editable {
+  my ($self,$new) = @_;
+  my $old = $self->{'_content_editable'};
+  $self->{'_content_editable'} = $new ? 1 : 0 if defined $new;
+  return $old;
+}
+
+sub _retrieve_content {
+  my($self) = @_;
+
+  my $textarea;
+  my %fields;
+
+  my $p = HTML::Parser->new( 'api_version' => 3,
+    start_h =>	[ sub {
+        my($elem,$attr) = @_;
+	return unless $attr->{'name'};
+	$textarea = \$fields{$attr->{'name'}} if $elem eq 'textarea';
+	$fields{$attr->{'name'}} = $attr->{'value'} if $elem eq 'input' && (lc $attr->{'type'}) eq 'hidden';
+      }, 'tagname, attr' ],
+    end_h   =>	[ sub {
+        my($elem) = @_;
+        $textarea = undef if $elem eq 'textarea';
+      }, 'tagname' ],
+    text_h  => 	[ sub {
+        return unless ref $textarea;
+	$$textarea .= shift;
+      }, 'dtext' ] );
+  
+  my $res = $self->_ua->get($self->_api('action'=>'edit','redirect'=>'no'), 
+      ':content_cb' => sub { $p->parse(shift); 1; });
+  $p->eof();
+
+  $self->{'_content_ref'} = \$fields{'wpTextbox1'}; delete $fields{'wpTextbox1'};
+  foreach(keys %fields) { $self->{'_content_edit_'.$_} = $fields{$_}; };
+  $self->{'_content_sha1'} = Digest::SHA1::sha1(${$self->{'_content_ref'}});
+}
+
+sub save {
+  my($self,$message,$minor) = @_;
+
+  carp 'read only - not editable' unless $self->editable;
+  carp 'no change message' unless $message;
+  carp 'no content' unless $self->{'_content_sha1'};
+  
+  # we can't have changed something if there has not been anyting downloaded to change
+  return 0 unless $self->{'_content_sha1'};	
+
+  # do nothing if the new and old content are identical
+  return 0 if $self->{'_content_sha1'} eq Digest::SHA1::sha1(${$self->{'_content_ref'}});
+
+  my %data = (); foreach(keys %{$self}) {
+    $data{$1} = $self->{$_} if m/_content_edit_(.*)/;
+  }
+
+  $data{'wpSave'} = 1; $data{'wpMinoredit'} = 1 if $minor;
+  $data{'wpSummary'} = $message;
+  $data{'wpTextbox1'} = ${$self->{'_content_ref'}};
+
+  my $res = $self->_ua->post($self->_api('action'=>'submit'), \%data);
+
+  if ($res->code >= 301 && $res->code <= 306) { 
+    # invalidate hash (=marker whether we have a current version)
+    delete $self->{'_content_sha1'}; return 1;
+  }
+  croak $res->message unless $res->is_success; croak 'update failed';
+
+  
+};
+
+1;
